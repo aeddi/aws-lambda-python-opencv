1187c39e49204449cd911ea0262be33bdcc5eb49
aeddi <antoine.e.b@gmail.com>
2016-11-16 17:21:55 +0100

    Add .editorconfig file

--- build.sh
+++ build.sh
@@ -16,18 +16,18 @@ cp -rf build/numpy/lib64/python2.7/site-packages/numpy lambda-package
 	git clone https://github.com/Itseez/opencv.git
 	cd opencv
 	git checkout 3.1.0
-	cmake						\
-		-D CMAKE_BUILD_TYPE=RELEASE		\
-		-D WITH_TBB=ON				\
-		-D WITH_IPP=ON				\
-		-D WITH_V4L=ON				\
-		-D ENABLE_AVX=ON			\
-		-D ENABLE_SSSE3=ON			\
-		-D ENABLE_SSE41=ON			\
-		-D ENABLE_SSE42=ON			\
-		-D ENABLE_POPCNT=ON			\
-		-D ENABLE_FAST_MATH=ON			\
-		-D BUILD_EXAMPLES=OFF			\
+	cmake										\
+		-D CMAKE_BUILD_TYPE=RELEASE				\
+		-D WITH_TBB=ON							\
+		-D WITH_IPP=ON							\
+		-D WITH_V4L=ON							\
+		-D ENABLE_AVX=ON						\
+		-D ENABLE_SSSE3=ON						\
+		-D ENABLE_SSE41=ON						\
+		-D ENABLE_SSE42=ON						\
+		-D ENABLE_POPCNT=ON						\
+		-D ENABLE_FAST_MATH=ON					\
+		-D BUILD_EXAMPLES=OFF					\
 		-D PYTHON2_NUMPY_INCLUDE_DIRS="$NUMPY"	\
 		.
 	make
--- template.py
+++ template.py
@@ -1,8 +1,8 @@
 import cv2
 
 def lambda_handler(event, context):
-    print "OpenCV installed version:", cv2.__version__
-    return "It works!"
+	print "OpenCV installed version:", cv2.__version__
+	return "It works!"
 
 if __name__ == "__main__":
-    lambda_handler(42, 42)
+	lambda_handler(42, 42)
